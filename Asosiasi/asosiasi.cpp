566923d202ed5082ff904f18d1a52dc00e06fbf4
Muhammad-Lutfi-RGB <muhammad.lutfi.ft24@mail.umy.ac.id>
2025-05-27 18:53:26 +0700

    menambahkan prosedur pada class dokter serta mendefinisikan prosedur tambah dokter dan cetak dokter

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -31,4 +31,20 @@ class dokter {
     ~dokter() {
         cout << "Dokter \"" << nama << "\" tidak ada\n";
     }
+    void tambahPasien(pasien*);
+    void cetakPasion();
 };
+
+void pasien::TambahDokter(dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+}
+
+void pasien::cetakDokter(){
+    cout << "Daftar dokter yang menangani pasien \""
+    << this->nama << "\":\n";
+    //auto digunakan dalam perulangan
+    for (auto& a : daftar_dokter) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
