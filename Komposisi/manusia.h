6503c472899a1ce9b8be804379289a0e8fc02b98
Muhammad-Lutfi-RGB <muhammad.lutfi.ft24@mail.umy.ac.id>
2025-05-27 19:49:42 +0700

    membuat file manusia serta membikin classnya

---
+++ Komposisi/manusia.h
@@ -0,0 +1,17 @@
+#ifndef MANUSIA_H
+#define MANUSIA_H
+using namespace std;
+
+class Manusia {
+    public:
+    string name;
+    jantung varJantung;
+
+    Manusia(string pName): name(pName){
+        cout << name << "Hidup\n";
+    }
+    ~Manusia(){
+        cout << name << "Mati\n";
+    }
+};
+#endif
